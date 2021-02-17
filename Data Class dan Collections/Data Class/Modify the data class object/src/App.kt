da684f229e3b88514a556724059a4fe93b550eca
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-02-17 21:08:45 +0700

    Finish data clases and collection

--- Data Class dan Collections/Data Class/Modify the data class object/src/App.kt
+++ Data Class dan Collections/Data Class/Modify the data class object/src/App.kt
@@ -1,4 +1,4 @@
-data class DataUser(val name : String, val age : Int)
+ data class DataUser(val name : String, val age : Int)
 
 fun main(){
     val dataUser = DataUser("nrohmen", 17)
@@ -6,7 +6,9 @@ fun main(){
     val dataUser3 = DataUser("dimas", 24)
     val dataUser4 = dataUser.copy()
     val dataUser5 = dataUser.copy(age = 18)
+    val dataUser6 = dataUser2.copy("joko")
 
     println(dataUser4)
     println(dataUser5)
+    println(dataUser6)
 }
